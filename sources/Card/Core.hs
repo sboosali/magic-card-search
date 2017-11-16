cffed4961d70fc30dc5deb5904407d32931e28a5
Spiros Boosalis <samboosalis@gmail.com>
2017-11-15 17:45:09 -0800

    `getDataFileName` proof-of-concept

--- sources/Card/Core.hs
+++ sources/Card/Core.hs
@@ -3,5 +3,22 @@ module Card.Core where
 import Card.Extra
 import Card.Types
 
+import Data.Aeson 
+
 import Prelude.Spiros
+
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy.Char8 as B8 
+import Paths_magic_card_search 
+
+cardFile :: FilePath
+cardFile = "data/SomeCards.json"
+
+getCards :: IO B.ByteString
+getCards = getDataFileName cardFile >>= B.readFile 
+
+printCards = getCards >>= B8.putStrLn
+
+parseCards cards = do 
+   return cards 
 
--- sources/Card/Example.hs
+++ sources/Card/Example.hs
@@ -1,12 +1,83 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-} -- to test inference
 module Card.Example where
-import Card()
+import Card
 import System.Environment
 
 {-|
 @
 stack build && stack exec -- example-magic-card-search
 @
+
+an example with a single card: 
+
+@
+{"Coiling Oracle":{"layout":"normal","name":"Coiling Oracle","manaCost":"{G}{U}","cmc":2,"colors":["Blue","Green"],"type":"Creature — Snake Elf Druid","types":["Creature"],"subtypes":["Snake","Elf","Druid"],"text":"When Coiling Oracle enters the battlefield, reveal the top card of your library. If it's a land card, put it onto the battlefield. Otherwise, put that card into your hand.","power":"1","toughness":"1","imageName":"coiling oracle","colorIdentity":["U","G"]}, ...}
+@
+
+which is pretty printed more readably as: 
+
+@
+{ "Coiling Oracle" : {
+    "layout" : normal,
+    "name" : Coiling Oracle,
+    "manaCost" : {G}{U},
+    "cmc" : 2,
+    "colors" : [
+        Blue,
+        Green
+        ],
+    "type" : Creature — Snake Elf Druid,
+    "types" : [
+        Creature
+        ],
+    "subtypes" : [
+        Snake,
+        Elf,
+        Druid
+        ],
+    "text" : When Coiling Oracle enters the battlefield, reveal the top card of your library. If it's a land card, put it onto the battlefield. Otherwise, put that card into your hand.,
+    "power" : 1,
+    "toughness" : 1,
+    "imageName" : coiling oracle,
+    "colorIdentity" : [
+        U,
+        G
+        ]
+    }
+}
+@
+
+some of these fields can be derived from others, so we can compress this into: 
+
+@
+{ "Coiling Oracle" : {
+    "layout" : normal,
+    "name" : Coiling Oracle,
+    "manaCost" : {G}{U},
+    "type" : Creature — Snake Elf Druid,
+    "text" : When Coiling Oracle enters the battlefield, reveal the top card of your library. If it's a land card, put it onto the battlefield. Otherwise, put that card into your hand.,
+    "power" : 1,
+    "toughness" : 1,
+    }
+}
+@ 
+
+or with slightly tighter invariants as: 
+
+@
+{ "Coiling Oracle" : {
+    "layout" : normal,
+    "name" : Coiling Oracle,
+    "cost" : {G}{U},
+    "type" : Creature — Snake Elf Druid,
+    "text" : When Coiling Oracle enters the battlefield, reveal the top card of your library. If it's a land card, put it onto the battlefield. Otherwise, put that card into your hand.,
+    "statistics" : [1, 1],
+    }
+}
+@ 
+
+
+
 -}
 main :: IO ()
 main = do
@@ -17,5 +88,5 @@ main = do
 
 mainWith s = do
  putStrLn s
- putStrLn "(Card.Example...)"
-
+ printCards 
+ -- getCards parseCards 
