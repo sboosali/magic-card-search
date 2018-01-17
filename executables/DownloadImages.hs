6b5b135e3e02b4995a4c0defc8f166f7ef606abb
Spiros Boosalis <SamBoosalis@gmail.com>
2018-01-17 14:19:10 -0800

    (1st commit from linux)

---
+++ executables/DownloadImages.hs
@@ -0,0 +1,11 @@
+
+import Control.Exception
+import System.Environment (getArgs)
+import Data.Monoid
+import Control.Monad
+import Data.Maybe
+
+import Control.Concurrent
+
+------------------------------------------
+
---
+++ shell.nix
@@ -0,0 +1,48 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, async, base, binary, bytestring
+      , containers, criterion, deepseq, directory, doctest, enumerate
+      , frisby, generic-lens, hashable, hspec, http-client
+      , http-client-tls, http-types, lens, lens-aeson, QuickCheck
+      , semigroups, show-prettyprint, spiros, stdenv, tar, text, time
+      , unordered-containers, vector, vinyl, wreq, zlib
+      }:
+      mkDerivation {
+        pname = "magic-card-search";
+        version = "0.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        enableSeparateDataOutput = true;
+        libraryHaskellDepends = [
+          aeson async base binary bytestring containers deepseq directory
+          enumerate frisby generic-lens hashable http-client http-client-tls
+          lens lens-aeson semigroups show-prettyprint spiros tar text time
+          unordered-containers vector vinyl zlib
+        ];
+        executableHaskellDepends = [
+          base bytestring http-client http-client-tls http-types lens
+          lens-aeson tar wreq zlib
+        ];
+        testHaskellDepends = [ base doctest hspec QuickCheck ];
+        benchmarkHaskellDepends = [ base criterion deepseq ];
+        homepage = "http://github.com/sboosali/magic-card-search#readme";
+        description = "TODO";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
